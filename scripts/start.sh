933904443e001aa25eee046c7c8d5a71dfaa8ee5
westzerosilver <oooiooop9988@gmail.com>
2024-11-20 21:27:07 +0900

    Chore: 수정 start.sh 기존 배포 파일 삭제 후 새 배포파일 생성으로 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,10 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+# build 파일 삭제
+echo "$TIME_NOW > $JAR_FILE 파일 삭제" >> $DEPLOY_LOG
+rm -f $JAR_FILE
+
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
